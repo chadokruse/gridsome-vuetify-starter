50fba2f1fa44c052edeccc2a7363ff5cc276e035
Chad Kruse <chadokruse@users.noreply.github.com>
2020-01-01 12:46:31 -0600

    Basic design tweaks

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,16 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <v-container fluid>
+      <v-row>
+        <v-col 
+          cols="12"
+          class="pa-12"
+        >
+          <h1>About us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </Layout>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,26 @@
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <v-container fluid>
+      <v-row>
+        <v-col 
+          cols="12"
+          class="pa-12"
+        >
+          <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+          <g-image alt="Example image" src="~/favicon.png" width="135" />
+          <h1>Gridsome Vuetify Starter</h1>
+          <p>
+            A quick starter template for my fellow non-designers
+          </p>
+          <p class="home-links">
+            <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+            <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+          </p>
+        </v-col>
+      </v-row>
 
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    </v-container>
 
   </Layout>
 </template>
@@ -21,7 +28,7 @@
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Gridsome Vuetify Starter Template'
   }
 }
 </script>
